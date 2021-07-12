bfe0ac2cdfa42c780431cfc9567ca6ffc872697e
Tomasz "TFKls" Kulis <tfk@tfkls.dev>
2021-07-13 00:40:29 +0200

    Added line numbering for files.

--- lisp/general-config.el
+++ lisp/general-config.el
@@ -1,5 +1,8 @@
 (setq make-backup-files nil)
 (setq auto-save-default nil)
 
+(when (version<= "26.0.50" emacs-version )
+  (global-display-line-numbers-mode))
+
 (provide 'general-config)
 ;;; general-config.el ends here
