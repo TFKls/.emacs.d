d57f08bd1b0d1313078269f24be334240d6b9f60
Tomasz "TFKls" Kulis <tfk@tfkls.dev>
2021-08-01 21:34:30 +0200

    Added working box-region utility function
    Added box-region, a function that surrounds a selected
    region of text with an ascii box. Example:
    
    Hello,
    world!
    
    turns into
    
    +--------+
    | Hello, |
    | world! |
    +--------+

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  '(haskell-stylish-on-save t)
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(rebecca-theme esup company yaml-mode dante company-mode markdown-mode pdf-tools flycheck-haskell haskell-mode use-package mu4e-alert gitignore-templates gitignore-mode flycheck))
+   '(xclip rebecca-theme esup company yaml-mode dante company-mode markdown-mode pdf-tools flycheck-haskell haskell-mode use-package mu4e-alert gitignore-templates gitignore-mode flycheck))
  '(xterm-mouse-mode t)
  '(xterm-mouse-utf-8 t))
 (custom-set-faces
--- lisp/utilities.el
+++ lisp/utilities.el
@@ -1,28 +1,22 @@
 (require 'use-package)
-
-(use-package s
-  :ensure t)
+(require 's)
+;; (use-package s
+;;  :ensure t)
 
 (defun box-region ($from $to)
-  (interactive (if (use-region-p)
-				   (list (region-beginning) (region-end))
-				 (let ((bds (bounds-of-thing-at-point 'sentence)) )
-				   (list nil (car bds) (cdr bds)) ) ) )
-  
-  (defvar strings (split-string (buffer-substring-no-properties $from $to) ("\n")))
-  (defvar strings-max-len (max (mapcar 'length strings)))
-  (defvar pad-max-len (apply-partially 's-center strings-max-len))
+  (interactive "r")
+  (let* ((strings (s-split "\n" (buffer-substring-no-properties $from $to)))
+  (strings-max-len (seq-reduce #'max (mapcar 'length strings) 0)))
 
-  (defun surround-pipe ($string)
-	(concat "| " (pad-max-len $string) " |\n"))
+	(defun surround-pipe ($string)
+	  (concat "| " (s-center strings-max-len $string) " |"))
 
-  
-  (save-excursion
-	(delete-region $from $to)
-	(goto-char $from)
-	(insert (concat "+-" (s-repeat strings-max-len "-") "-+\n"))
-	(insert (concat (mapcar 'surround-pipe strings))) 
-	(insert (concat "+-" (s-repeat strings-max-len "-") "-+\n"))))
+  	(save-excursion
+	  (delete-region $from $to)
+	  (goto-char $from)
+	  (insert (concat "+-" (s-repeat strings-max-len "-") "-+\n"
+					  (mapconcat 'surround-pipe strings "\n")
+					  "\n+-" (s-repeat strings-max-len "-") "-+")))))
 
 (provide 'utilities)
 ;;; utilities.el ends here
